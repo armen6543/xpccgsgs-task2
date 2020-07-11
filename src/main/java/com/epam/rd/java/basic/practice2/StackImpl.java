4885848c9f68f1629751a4ef3523fdac2c940994
Armen <sportnutrnew@gmail.com>
2020-07-12 02:10:53 +0300

    Update StackImpl.java

--- src/main/java/com/epam/rd/java/basic/practice2/StackImpl.java
+++ src/main/java/com/epam/rd/java/basic/practice2/StackImpl.java
@@ -3,15 +3,27 @@ package com.epam.rd.java.basic.practice2;
 import java.util.Iterator;
 
 public class StackImpl implements Stack {
+    private int maxSize;
+    private Object[] stackArray;
+    private int count;
+
+    public StackImpl(int max) {
+        this.maxSize = max;
+        stackArray = new Object[maxSize];
+        count = 0;}
 
     @Override
     public void clear() {
+        for (int i = 0; i < stackArray.length; i++) {
+            stackArray[i] = null;
+        }
+        count=0;
         
     }
 
     @Override
     public int size() {
-        return 0;
+        return count;
     }
 
     public Iterator<Object> iterator() {
@@ -22,37 +34,57 @@ public class StackImpl implements Stack {
 
         @Override
         public boolean hasNext() {
-            return false;
+            return (count < stackArray.length);
         }
 
         @Override
         public Object next() {
-            return null;
+            return stackArray[count++];
         }
 
     }
 
     @Override
     public void push(Object element) {
+        stackArray[count] = element;
+        count++;
         
     }
 
     @Override
     public Object pop() {
-        return null;
+        Object result = stackArray[--count];
+        stackArray[count] = null;
+        return result;
     }
 
     @Override
     public Object top() {
-        return null;
+        return stackArray[count-1];
     }
 
     @Override
     public String toString() {
-        return null;
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+        for (int i = 0; i < stackArray.length - 1; i++) {
+            s.append(stackArray[i] + ", ");
+        }
+        s.append(stackArray[stackArray.length - 1]);
+        s.append("]");
+        return s.toString();
     }
 
     public static void main(String[] args) {
+        StackImpl stack=new StackImpl(10);
+        stack.push(34);
+        stack.push(56);
+        System.out.println(stack);
+        System.out.println(stack.top());
+        stack.pop();
+        System.out.println(stack);
+
+
 
     }
 

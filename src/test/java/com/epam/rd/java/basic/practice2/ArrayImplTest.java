95c47c1cd5549e0fd9af7ea5587e4ccb9c4fab25
XpCcgsGs <xpccgsgs@email.com>
2020-07-12 22:48:08 +0000

    Update ArrayImplTest.java

--- src/test/java/com/epam/rd/java/basic/practice2/ArrayImplTest.java
+++ src/test/java/com/epam/rd/java/basic/practice2/ArrayImplTest.java
@@ -1,20 +1,7 @@
 package com.epam.rd.java.basic.practice2;
 
-
-import org.junit.Assert;
-import org.junit.Test;
-
 public class ArrayImplTest {
 
-     @Test
-    public void CheckGet() {
-        ArrayImpl array=new ArrayImpl();
-        array.add(2);
-        array.add(3);
-        Object s=array.get(1);
-        Assert.assertEquals(3,s);
-
-    }
 
 
 }

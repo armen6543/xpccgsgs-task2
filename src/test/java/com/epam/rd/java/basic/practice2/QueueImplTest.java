3f0c741903b43571aeac36df85ec01658f26361c
Armen <sportnutrnew@gmail.com>
2020-07-14 02:29:53 +0300

    Update QueueImplTest.java

--- src/test/java/com/epam/rd/java/basic/practice2/QueueImplTest.java
+++ src/test/java/com/epam/rd/java/basic/practice2/QueueImplTest.java
@@ -1,5 +1,17 @@
 package com.epam.rd.java.basic.practice2;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class QueueImplTest {
+
+    @Test
+    public void CheckTop() {
+        QueueImpl queue=new QueueImpl();
+        queue.enqueue(45);
+        queue.enqueue(67);
+        Object s=queue.top();
+        Assert.assertEquals(45,s);
+    }
 
 }

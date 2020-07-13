08874388f644a95dab8e88bfea88928d652ec8ee
Armen <sportnutrnew@gmail.com>
2020-07-14 02:29:24 +0300

    Update ListImplTest.java

--- src/test/java/com/epam/rd/java/basic/practice2/ListImplTest.java
+++ src/test/java/com/epam/rd/java/basic/practice2/ListImplTest.java
@@ -1,5 +1,17 @@
 package com.epam.rd.java.basic.practice2;
 
-public class ListImplTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class ListImplTest {
+    @Test
+    public void CheckTop() {
+        ListImpl list = new ListImpl();
+        list.addFirst(56);
+        list.addFirst(89);
+        list.addFirst(34);
+        Object s = list.size();
+        Assert.assertEquals(3, s);
+
+    }
 }

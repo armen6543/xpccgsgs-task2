c0e0d3db843ac0e992b1b933408439894cb36f71
Armen <sportnutrnew@gmail.com>
2020-07-14 02:29:06 +0300

    Update Demo.java

--- src/main/java/com/epam/rd/java/basic/practice2/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice2/Demo.java
@@ -1,4 +1,11 @@
 package com.epam.rd.java.basic.practice2;
 
 public class Demo {
+    public static void main(String[] args) {
+        StackImpl stack=new StackImpl();
+        stack.push(34);
+        stack.push(56);
+        Object s=stack.top();
+        System.out.println(s);
+    }
 }

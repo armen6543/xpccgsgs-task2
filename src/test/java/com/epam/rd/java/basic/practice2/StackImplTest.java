c210345e77cc375aad8c8583b0e13fddb61c8c79
Armen <sportnutrnew@gmail.com>
2020-07-14 02:30:18 +0300

    Update StackImplTest.java

--- src/test/java/com/epam/rd/java/basic/practice2/StackImplTest.java
+++ src/test/java/com/epam/rd/java/basic/practice2/StackImplTest.java
@@ -1,5 +1,17 @@
 package com.epam.rd.java.basic.practice2;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StackImplTest {
+
+    @Test
+    public void CheckTop() {
+        StackImpl stack=new StackImpl();
+        stack.push(34);
+        stack.push(56);
+        Object s=stack.top();
+        Assert.assertEquals(56,s);
+    }
 
 }
